a05cf0bb2f898705320ff0013cfcba851e2a7264
unknown <subhashnaidu77@gmail.com>
2024-03-26 18:21:05 +0530

    first commit

---
+++ app/src/AccordionComponent.js
@@ -0,0 +1,65 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Accordion from 'react-bootstrap/Accordion';
+
+import './Accordion.css';
+function AccordionComponent() {
+
+
+  
+    return (
+        
+            <Accordion alwaysOpen>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+        
+
+
+            <div>
+      <Accordion.Item eventKey="1" >
+        <Accordion.Header  >Accordion Item #2</Accordion.Header>
+        <Accordion.Body >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      </div>
+      <div>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Accordion Item #3</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      </div>
+    </Accordion>
+
+
+            
+
+        
+    );
+};
+
+export default AccordionComponent;
---
+++ app/src/App.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from 'react';
+import AccordionComponent from './AccordionComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AccordionComponent/>
+
+   
+     
+    </div>
+  );
+}
+
+export default App;
